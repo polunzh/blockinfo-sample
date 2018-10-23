3f426a74538014b0acdbb0deec10c88ec14ad1c6
zhangzhenqiang <zhangzhenqiang@weego.me>
2018-10-23 12:12:44 +0800

    add ajax error handle

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import red from '@material-ui/core/colors/red';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import axios from 'axios';
 
 import SearchBar from './components/SearchBar';
 import Summary from './components/Summary';
 import Transactions from './components/Transactions';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    textAlign: 'center',
-  },
-};
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       hash: '000000000000000001806a922d4d35a37ad9324c690f72d556c6445cb7a9c214',
-      summary: {},
+      summary: null,
+      errmsg: '',
     };
+  }
+
+  resetErrmsg() {
+    this.setState({ errmsg: '' });
   }
 
   async search(e) {
@@ -29,17 +27,29 @@ class App extends Component {
       return false;
     }
 
+    this.resetErrmsg();
     const hash = this.state.hash.trim();
     if (hash === '') {
       return false;
     }
 
-    const resp = await axios.get(`/rawblock/${this.state.hash}`, {
-      headers: { 'Cache-Control': 'no-cache' },
-    });
+    try {
+      const hash = this.state.hash.trim();
+      if (hash === '') {
+        return false;
+      }
 
-    this.setState({ summary: resp.data });
-    this.setState({ transactions: resp.data.tx });
+      const resp = await axios.get(`/rawblock/${this.state.hash}`, {
+        headers: { 'Cache-Control': 'no-cache' },
+      });
+
+      const { tx: transactions, ...summary } = resp.data;
+      this.setState({ summary });
+      this.setState({ transactions });
+    } catch (error) {
+      console.error(`search error: ${error.message}`);
+      this.setState({ errmsg: 'Load data failed!' });
+    }
   }
 
   hashInput(e) {
@@ -55,10 +65,21 @@ class App extends Component {
           hash={this.state.hash}
         />
         <Grid container justify="center">
-          <Grid item xs={10}>
-            <Summary summary={this.state.summary} />
-            <Transactions transactions={this.state.transactions} />
-          </Grid>
+          {this.state.errmsg && (
+            <Grid item>
+              <SnackbarContent
+                style={{ backgroundColor: red[600] }}
+                message={this.state.errmsg}
+              />
+            </Grid>
+          )}
+          {this.state.summary &&
+            this.state.transactions && (
+              <Grid item xs={10}>
+                <Summary summary={this.state.summary} />
+                <Transactions transactions={this.state.transactions} />
+              </Grid>
+            )}
         </Grid>
       </div>
     );
